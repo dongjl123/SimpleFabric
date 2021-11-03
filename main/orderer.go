f043b177205d863e52bad821b1083ec0e48ddd7b
bigbigbigface <847767026@qq.com>
2021-11-03 22:56:07 +0800

    edit code about order's create and serve, and add order push block to PrimaryPeer

--- main/orderer.go
+++ main/orderer.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	fb "github.com/SimpleFabric/fabric"
+	"os"
+)
+
+var (
+	ordercmd     string
+	orderorg     string
+	orderid      string
+	maxblocksize int
+)
+
+func init() {
+	flag.StringVar(&ordercmd, "c", "", "input command, must be order")
+	flag.StringVar(&orderorg, "o", "", "the order's organization")
+	flag.StringVar(&orderid, "i", "", "the order's id")
+	flag.IntVar(&maxblocksize, "m", 100, "the max block size")
+}
+
+//输入命令参数，组织名，order名
+func main() {
+	flag.Parse()
+
+	if ordercmd != "peer" {
+		fmt.Fprintf(os.Stderr, "The first flag must be order...\n")
+		os.Exit(1)
+	}
+
+	if orderorg == "" || orderid == "" {
+		fmt.Fprintf(os.Stderr, "Peer argument loss...\n")
+		os.Exit(1)
+	}
+	o, _ := fb.NewOrder(orderorg, orderid, maxblocksize)
+	go o.Server()
+}
